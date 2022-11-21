a2d0591c36b0b1ab7c3e5bd28f413dc762596ac0
Luofan Chen <clfbbn@gmail.com>
2022-11-21 20:34:24 +0800

    [SQUASH] Merge linux 4.19.262 and erofs-backport
    This commit is a squashed merge of the following repositories:
    
    Repo: https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux-stable.git
    Tag: v4.19.262
    Repo: https://github.com/erofs/erofs-backports
    Branch: linux-4.19.y
    Change-Id: If028bdc4ad3e9b507afb1b580c4c55bf5f5c8ef1

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -920,7 +920,7 @@ static ssize_t read_block_state(struct file *file, char __user *buf,
 			zram_test_flag(zram, index, ZRAM_HUGE) ? 'h' : '.',
 			zram_test_flag(zram, index, ZRAM_IDLE) ? 'i' : '.');
 
-		if (count < copied) {
+		if (count <= copied) {
 			zram_slot_unlock(zram, index);
 			break;
 		}
@@ -2086,8 +2086,7 @@ static int zram_add(void)
 
 	zram->disk->queue->backing_dev_info->capabilities |=
 			(BDI_CAP_STABLE_WRITES | BDI_CAP_SYNCHRONOUS_IO);
-	disk_to_dev(zram->disk)->groups = zram_disk_attr_groups;
-	add_disk(zram->disk);
+	device_add_disk(NULL, zram->disk, zram_disk_attr_groups);
 
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 
@@ -2123,6 +2122,7 @@ static int zram_remove(struct zram *zram)
 	mutex_unlock(&bdev->bd_mutex);
 
 	zram_debugfs_unregister(zram);
+
 	/* Make sure all the pending I/O are finished */
 	fsync_bdev(bdev);
 	zram_reset_device(zram);
